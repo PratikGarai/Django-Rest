e3e83d3f3576ae04004f82b73d53a8d0c0dd85c3
Pratik Garai <pratikgarai0208@gmail.com>
2020-08-03 01:43:39 +0530

    Changed serializer to model serializer

--- restproject/snippets/serializers.py
+++ restproject/snippets/serializers.py
@@ -1,23 +1,7 @@
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import Snippet
 
 class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerFiels(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template':'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('title', instance.code)
-        instance.linenos = validated_data.get('title', instance.linenos)
-        instance.language = validated_data.get('title', instance.language)
-        instance.style = validated_data.get('title', instance.style)
-        instance.save()
-        return instance
-
+    class Meta:
+        model = Snippet
+        fields = [ 'id', 'title', 'code', 'linenos', 'language', 'style' ]
