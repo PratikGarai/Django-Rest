4c90f5456ce8adf39740684421cecdcf4265bd60
Pratik Garai <pratikgarai0208@gmail.com>
2020-08-09 01:21:52 +0530

    Added the detail views

--- restproject/snippets/views.py
+++ restproject/snippets/views.py
@@ -19,3 +19,26 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status = 201)
         return JsonResponse(serializer.errors , status =400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    try :
+        snippet = Snippet.objects.all(pk=pk)
+    except Snippet.DoesNotExist :
+        return HttpResponse(status = 404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method=='PUT':
+        data = JSONParser.parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializers.error, status = 400)
+
+    elif request.method=='DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
